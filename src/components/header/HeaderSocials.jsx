741b28a112700c7545adba9d606325f3df75e5b2
jvalen01 <villafifa@outlook.com>
2023-09-01 21:40:48 +0200

    Made folders for all components and finished header component

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://www.linkedin.com/in/jonatan-valen-854045235/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/jvalen01" target="_blank"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/jonatan-valen-854045235/" target="_blank"><BsLinkedin/></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
