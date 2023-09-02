b1dbfc36c5bf3518d8b5965d77d6f2715c94b292
jvalen01 <villafifa@outlook.com>
2023-09-02 14:52:50 +0200

    Finished experience component and problem with image background

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,114 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { AiFillHtml5 } from "react-icons/ai";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa";
+
+import { FaJava } from "react-icons/fa";
+import { SiCsharp } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMysql } from "react-icons/si";
+import { SiFlask } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>The SKills I have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <AiFillHtml5 className="experience__details-icon"/>
+              <div>
+                <h4>HTML5</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCss3Alt className="experience__details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiJavascript className="experience__details-icon"/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaReact className="experience__details-icon"/>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaBootstrap className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaJava className="experience__details-icon"/>
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiCsharp className="experience__details-icon"/>
+              <div>
+                <h4>C#</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaPython className="experience__details-icon"/>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaNodeJs className="experience__details-icon"/>
+              <div>
+                <h4>NodeJS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiMysql className="experience__details-icon"/>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiFlask className="experience__details-icon"/>
+              <div>
+                <h4>Flask</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Experience
+export default Experience;
